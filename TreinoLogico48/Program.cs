a0846543dd125471d3823b32be3290c03072f0ac
Luiz Henrique Rocha <henrique.rochas@hotmail.com>
2025-10-30 12:11:26 -0300

    Atualização 10 # - Treino de Logica: Exercicios 46 ao 50.
    Os exercícios compõem:
     - Lógica condicional,
     - Loop (For, While),
     - Arrays

---
+++ TreinoLogico48/Program.cs
@@ -0,0 +1,47 @@
+﻿//1099
+
+namespace TreinoLogico48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maiorNumero = 0;
+            int menorNumero = 0;
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int somaImpares = 0;
+                string valores = Console.ReadLine();
+
+                string[] yx = valores.Split(' ');
+
+                int x = int.Parse(yx[0]);
+                int y = int.Parse(yx[1]);
+
+                if (x > y)
+                {
+                    maiorNumero = x;
+                    menorNumero = y;
+                }
+                else
+                {
+                    maiorNumero = y;
+                    menorNumero = x;
+                }
+
+                for (int j = menorNumero + 1; j < maiorNumero; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        somaImpares += j;
+                        Console.WriteLine(somaImpares);
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ TreinoLogico49/Program.cs
@@ -0,0 +1,48 @@
+﻿// 1101
+namespace TreinoLogico49
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maiorNum;
+            int menorNum;
+            bool parada = true;
+
+            while (parada == true)
+            {
+                int soma = 0;
+                string pares = Console.ReadLine();
+                string[] mn = pares.Split(' ');
+                int n = int.Parse(mn[0]);
+                int m = int.Parse(mn[1]);
+
+                if (n <= 0 || m <= 0)
+                {
+                    parada = false;
+                    break;
+                }
+
+                if(n > m)
+                {
+                    maiorNum = n;
+                    menorNum = m;
+                }
+                else
+                {
+                    maiorNum = m;
+                    menorNum = n;
+                }
+
+                for(int j = menorNum; j <= maiorNum; j++)
+                {
+                    soma += j;
+                    Console.Write($"{j} ");
+                }
+
+                Console.WriteLine($"Sum = { soma}");
+            }
+
+        }
+    }
+}
